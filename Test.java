d8cebe9bc5b57f2f57a1b8d7fe9d457f568d9cea
gokulrajrsp <121859239+gokulrajrsp@users.noreply.github.com>
2023-04-03 12:11:49 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+package string;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		for(int i=1;i<=7;i++) {
+			System.out.print("*"+a);
+			a=(a*10)+b;
+			b++;
+		}
+
+	}
+
+}
